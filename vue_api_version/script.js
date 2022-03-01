4f959a69688e1730a774fb72e31fe7afad389bdc
marcodeiulio <mdeiulio@icloud.com>
2022-03-01 20:40:53 +0100

    - Completed vue_api_version

--- vue_api_version/script.js
+++ vue_api_version/script.js
@@ -5,9 +5,16 @@ Vue.config.devtools = true;
 const app = new Vue({
 	el: '#root',
 	data: {
-
+		albums: [],
+	},
+	methods: {
 	},
 	mounted() {
-
+		{
+			axios.get('http://localhost:8888/php-ajax-dischi/vue_api_version/api/api_dischi.php').then(res => {
+				this.albums = res.data;
+				console.log(this.albums);
+			}).catch(err => { console.log(err) });
+		}
 	}
 })
